d4fccea5ae79fb85dc8da705f7275dba138c1607
mon <amrin0616@gmail.com>
2025-07-08 19:44:25 +0700

    feat: little navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,9 +18,11 @@ const Navbar = () => {
 
   return (
     <nav className={`fixed top-0 left-0 right-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5`}>
-      <Link>
+      <div className="flex items-center gap-1">
         <img src={Logo} alt="Logo" className="w-13 h-auto" />
-      </Link>
+        <span className="text-4xl font-bold text-amber-50">Nobi.</span>
+      </div>
+      
 
       <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
         <XIcon className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
